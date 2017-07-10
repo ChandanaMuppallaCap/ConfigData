b25006a1a34a2a5a546ed99848ee49516c7f4ef7
chandana muppalla <chandana.muppalla@capgemini.com>
2017-07-10 10:27:11 -0400

    Config Server with Data

---
+++ src/main/java/CloudServer/ServerCloud/ServerCloudApplication.java
@@ -0,0 +1,12 @@
+package CloudServer.ServerCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerCloudApplication.class, args);
+	}
+}
